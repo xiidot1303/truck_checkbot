2fb49abe33abe4ac9cf53b2f28c9afdc1597c635
xiidot1303 <xii1303@inbox.ru>
2024-06-11 15:30:13 +0500

    change setting lang to ru

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ SESSION_SAVE_EVERY_REQUEST = True
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Asia/Tashkent'
 
