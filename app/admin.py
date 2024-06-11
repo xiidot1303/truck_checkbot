4894dfe0dcb8ddea9726f48ad6988ce8dab26f60
xiidot1303 <xii1303@inbox.ru>
2024-06-11 15:30:00 +0500

    add tasks models

--- app/admin.py
+++ app/admin.py
@@ -9,5 +9,20 @@ class CarAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
 
+class TaskDepotInline(admin.TabularInline):
+    model = TaskDepot
+    extra = 1  # Number of extra forms to display
+    ordering = ['order']  # Order by 'order' field
+
+class TaskEventInline(admin.TabularInline):
+    model = TaskEvent
+    extra = 1
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [TaskDepotInline, TaskEventInline]
+
 admin.site.register(Depot, DepotAdmin)
 admin.site.register(Car, CarAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskDepot)
+admin.site.register(TaskEvent)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Depot(models.Model):
     code = models.IntegerField(null=True, blank=False)
@@ -7,9 +8,47 @@ class Depot(models.Model):
     tg_id = models.CharField(null=True, blank=True, max_length=16)
     bot_user = models.OneToOneField('bot.DepotManager', null=True, blank=True, on_delete=models.PROTECT)
 
+    def __str__(self) -> str:
+        return f"{self.branch} | {self.title}"
+
 class Car(models.Model):
     code = models.CharField(null=True, blank=False, max_length=64)
     title = models.CharField(null=True, blank=True, max_length=255)
     number = models.CharField(null=True, blank=True, max_length=64)
     tg_id = models.CharField(null=True, blank=True, max_length=16)
     bot_user = models.OneToOneField('bot.Driver', null=True, blank=True, on_delete=models.PROTECT)
+
+class Task(models.Model):
+    driver = models.ForeignKey('bot.Driver', on_delete=models.CASCADE)
+    depots = models.ManyToManyField(Depot, through='TaskDepot')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_complete = models.BooleanField(default=False)
+    current_depot_index = models.PositiveIntegerField(default=0)
+
+    def get_next_depot(self):
+        depot_relations = self.taskdepot_set.order_by('order')
+        if self.current_depot_index < depot_relations.count():
+            return depot_relations[self.current_depot_index].depot
+        return None
+
+class TaskDepot(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    depot = models.ForeignKey(Depot, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('task', 'depot')
+
+class TaskEvent(models.Model):
+    EVENT_TYPES = [
+        ('in_factory', 'In Factory'),
+        ('factory_done', 'Factory Done'),
+        ('arrived_at_depot', 'Arrived at Depot'),
+        ('depot_done', 'Depot Done'),
+    ]
+
+    task = models.ForeignKey(Task, related_name='events', on_delete=models.CASCADE)
+    event_type = models.CharField(max_length=50, choices=EVENT_TYPES)
+    start_time = models.DateTimeField(null=True, blank=True) 
+    end_time = models.DateTimeField(null=True, blank=True)
+    depot = models.ForeignKey(Depot, null=True, blank=True, on_delete=models.CASCADE)
