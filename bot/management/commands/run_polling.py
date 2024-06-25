60d3329af42bc6719a0a6e85839c72297172bf3f
xiidot1303 <xii1303@inbox.ru>
2024-06-25 14:16:43 +0500

    make run_polling for two bots

--- bot/management/commands/run_polling.py
+++ bot/management/commands/run_polling.py
@@ -1,11 +1,25 @@
 from django.core.management.base import BaseCommand
-from bot.control.updater import application
+from bot.driver.control.updater import application as driver_app
+from bot.depot_manager.control.updater import application as depot_manager_app
 import asyncio
 
 class Command(BaseCommand):
     help = 'Command to polling bot'
 
+    def add_arguments(self, parser):
+        # Adding a positional argument
+        parser.add_argument('bot', type=str, help='Bot name')
+
     def handle(self, *args, **options):
-        asyncio.run(
-            application.run_polling()
-        )
+        bot = options['bot']
+        if bot == 'driver':
+            asyncio.run(
+                driver_app.run_polling()
+            )
+        elif bot == 'depot_manager':
+            asyncio.run(
+                depot_manager_app.run_polling()
+            )
+        else:
+            print("Bot not found")
+        
