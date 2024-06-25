52c626e9a2cbfbd8b4e1c193f7d8a0f6796bf582
xiidot1303 <xii1303@inbox.ru>
2024-06-25 14:41:03 +0500

    fix urlspatterns

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('', include('bot.urls')),
-]
-
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
