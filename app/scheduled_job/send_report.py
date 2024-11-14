5e98bf1c54556e886736a86ce6ff5a29230f421c
Zzior <xasan10102020@gmail.com>
2024-11-14 10:23:40 +0500

    correct send_report

--- app/scheduled_job/send_report.py
+++ app/scheduled_job/send_report.py
@@ -7,8 +7,7 @@ from telegram import InputFile
 from bot.models import ReportGroup
 from app.views.task import create_task_report
 from bot.services.driver_service import get_report_group
-from bot.driver.control.updater import application as driver_app
-
+from bot.utils.bot_functions import driver_app, send_newsletter
 
 async def send_report():
     now = datetime.now()
@@ -21,9 +20,11 @@ async def send_report():
     file_report.seek(0)
 
     group: ReportGroup = await get_report_group()
-    await driver_app.bot.send_document(
+    await send_newsletter(
+        driver_app.bot,
         group.tg_id_2,
-        InputFile(file_report, filename=f"{now.strftime('%Y_%m_%d')}_.xlsx")
+        None,
+        document=InputFile(file_report, filename=f"{now.strftime('%Y_%m_%d')}_.xlsx")
     )
 
 
